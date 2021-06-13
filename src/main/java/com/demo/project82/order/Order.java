fb6e21958fe5bc7bb2db5ae5e244793e41bcf4f7
Arjun Surendra <asurendra@vmware.com>
2021-06-13 16:28:34 +0530

    code

--- src/main/java/com/demo/project82/order/Order.java
+++ src/main/java/com/demo/project82/order/Order.java
@@ -2,16 +2,13 @@ package com.demo.project82.order;
 
 import java.util.Map;
 import javax.persistence.CascadeType;
-import javax.persistence.CollectionTable;
 import javax.persistence.Column;
-import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.MapKey;
-import javax.persistence.MapKeyColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
@@ -20,6 +17,15 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+/**
+ * https://javakeypoint.wordpress.com/2020/04/21/difference-between-joincolumn-and-jointable-in-hibernate/
+ * https://stackoverflow.com/questions/30288464/when-should-i-use-joincolumn-or-jointable-with-jpa
+ * https://medium.com/nerd-for-tech/elementcollection-vs-onetomany-in-hibernate-7fb7d2ac00ea
+ * https://thorben-janssen.com/ultimate-guide-association-mappings-jpa-hibernate/
+ * https://www.baeldung.com/jpa-many-to-many
+ * https://www.callicoder.com/hibernate-spring-boot-jpa-element-collection-demo/
+ * https://vladmihalcea.com/blog/
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
